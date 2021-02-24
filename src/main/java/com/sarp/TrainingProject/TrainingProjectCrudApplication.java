3fc05ae57514df456ad69f1f2c7977998baaea0d
Prasetyo Adi Nugroho <prasetyoadin@gmail.com>
2021-02-24 09:29:30 +0700

    Add files via upload
    my first commit from TrainingProject-CRUD Java

---
+++ src/main/java/com/sarp/TrainingProject/TrainingProjectCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sarp.TrainingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingProjectCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingProjectCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarp/TrainingProject/TrainingProjectCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarp.TrainingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingProjectCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
